12b3b5811be5a6fc2a7ac992c8df7c612c29ebe7
thomascomander9-sys <thomascomander9@gmail.com>
2026-08-31 06:35:15 +0300

    Create product_model.dart

---
+++ lib/product_model.dart
@@ -0,0 +1,32 @@
+class Product {
+  final int? id;
+  final String name;
+  final double price;
+  final int stock;
+
+  Product({
+    this.id,
+    required this.name,
+    required this.price,
+    required this.stock,
+  });
+
+  Map<String, dynamic> toMap() {
+    return {
+      'id': id,
+      'name': name,
+      'price': price,
+      'stock': stock,
+    };
+  }
+
+  factory Product.fromMap(Map<String, dynamic> map) {
+    return Product(
+      id: map['id'],
+      name: map['name'],
+      price: map['price'],
+      stock: map['stock'],
+    );
+  }
+}
+
